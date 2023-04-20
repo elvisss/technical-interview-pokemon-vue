110358ee27f4672564c4e1fd39e93a4426e3af98
Elvis Saavedra <elvis.saavedra.segura@gmail.com>
2023-04-20 18:44:41 -0500

    adding code formatting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,12 +3,21 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/eslint-config-typescript'
+    '@vue/eslint-config-typescript',
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/multi-word-component-names': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+  },
 }
